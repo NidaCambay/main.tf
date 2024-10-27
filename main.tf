f42509223e91dc68d99899d979c4904662c95907
NidaCambay <cambayn@gmail.com>
2024-10-27 23:36:52 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,19 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+    required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.65.0"
+    }
+
+    github = {
+        source = "integrations/github"
+        version = "6.2.3"
+    }
+    }
 }
 
-resource "aws_instance" "task-1" {
-  ami = lookup(var.myami, terraform.workspace)
-  instance_type = lookup(var.instance_type, terraform.workspace)
-  count = 2
-  key_name = lookup(var.key-pem, terraform.workspace)
-  security_groups = [aws_security_group.brc-sg.name]
-  tags = {
-    Name = "${terraform.workspace}-server"
-  }
+provider "aws" {
+    region = "us-east-1"
 }
 
 resource "aws_security_group" "brc-sg" {
@@ -34,37 +37,26 @@ resource "aws_security_group" "brc-sg" {
     }
 }
 
-variable "myami" {
-  type = map(string)
-  default = {
-    dev     = "ami-06640050dc3f556bb"
-    prod    = "ami-08d4ac5b634553e16"
-    test    = "ami-0cff7528ff583bf9a"
-    staging = "ami-0ddc798b3f1a5117e"
+variable "ins_ami" {}
+
+variable "ins_type" {}
+
+variable "keypair" {}
+
+
+resource "aws_instance" "tfmyec2" {
+  ami = var.ins_ami
+  instance_type = var.ins_type  #lookup haricinde bu sekilde de kullanilabilir, ornek olmasi icin yaptim
+  key_name = var.keypair
+  security_groups = [aws_security_group.brc-sg.name]
+
+  tags = {
+    Name = "${terraform.workspace}-server"
+
   }
 }
 
-
-variable "instance_type" {
-  type = map(string)
-  default = {
-    dev     = "t2.micro"
-    prod    = "t2.nano"
-    test    = "t3a.medium"
-    staging = "t2.small"
-  }
-}
-
-variable "key-pem" {
-  type = map(string)
-  default = {
-    dev     = "dev-key"
-    prod    = "prod-key"
-    test    = "test-key"
-    staging = "staging-key"
-  }
-}
-
-output "instance_public_ips" {
-  value = [for instance in aws_instance.task-1 : instance.public_ip]
+output "workspace_instance_ip" {
+  description = "Public IP"
+  value       = "${terraform.workspace}-instance ip: ${aws_instance.tfmyec2.public_ip}"
 }
